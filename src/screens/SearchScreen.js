f9048417c957db2297a76c96f0812b14754983f7
Hisan Ibrahim <hisan@creatnlrn.com>
2020-01-09 10:54:32 +0530

    added some boiler plate for navigation and search screen

---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Text,View,StyleSheet} from 'react-native';
+
+const SearchScreen = () => {
+    return <View>
+        <Text>Search Screen</Text>
+    </View>
+}
+
+export default SearchScreen
