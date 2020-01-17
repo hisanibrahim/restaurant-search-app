d2856d937acbd7c41a5a3e409465ad500cf4e429
Hisan Ibrahim <hisan@creatnlrn.com>
2020-01-17 19:07:19 +0530

    initial request with useEffect hook

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Text, View, StyleSheet } from "react-native";
 import SearchBar from "../components/SearchBar";
 import zomato from "../api/zomato";
@@ -8,27 +8,34 @@ const SearchScreen = () => {
   const [results, setResults] = useState([]);
   const [errorMessage, setErrorMessage] = useState("");
 
-  const searchApi = async () => {
+  const searchApi = async searchTerm => {
     try {
+      console.log("Hi there!");
       setErrorMessage("");
       const response = await zomato.get("/search", {
         params: {
-          q: term // Zomato API parameter for search term
+          q: searchTerm // Zomato API parameter for search term
         }
       });
       setResults(response.data.restaurants);
-      console.log(results.length);
     } catch (err) {
       setErrorMessage("Something went wrong! Please try again.");
     }
   };
+
+  useEffect(() => {
+    searchApi("s");
+  }, []);
+
+  // adding a empty array '[]' as second parameter run
+  // first parameter function only once
 
   return (
     <View>
       <SearchBar
         term={term}
         onTermChange={newTerm => setTerm(newTerm)}
-        onTermSubmit={() => searchApi()}
+        onTermSubmit={() => searchApi(term)}
       />
       {errorMessage ? <Text>{errorMessage}</Text> : null}
       <Text>We have found {results.length} results.</Text>
